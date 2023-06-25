efffb0b659447ca6be0c415e5096ed6d6a22d7b4
malla sagar <mallasagar0101@gmail.com>
2023-06-25 18:08:29 +0545

    configure db

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+const mongodb=require("mongodb");
+const MongoClient=mongodb.MongoClient;
+const dbUrl =  "mongodb://127.0.0.1:27017"
+const dbName = "art"
+
+router.route('/')
+.post(function(req,res,next){
+  // conntecting a databases
+  MongoClient.connect(dbUrl)
+    .then((client)=>{
+      // when connection is success
+      const db=client.db(dbName);
+      db.collection('art').insertOne(req.body)
+      .then((success)=>{
+        res.json(success)
+      })
+      .catch((err)=>{
+        res.json(err)
+      })
+
+    })
+    .catch((err)=>{
+      res.json({error:err})
+    })
+})
+.get(function(req,res,next){
+  // conntecting a databases
+  MongoClient.connect(dbUrl)
+    .then((client)=>{
+      // when connection is success
+      const db=client.db(dbName);
+      db.collection('art')
+      .find()
+      .toArray()
+      .then((art)=>{
+        res.json(art)
+
+      })
+      .catch((err)=>{
+        res.json(err)
+      })
+
+    })
+    .catch((err)=>{
+      res.json({error:err})
+    })
+})
 
 module.exports = router;
